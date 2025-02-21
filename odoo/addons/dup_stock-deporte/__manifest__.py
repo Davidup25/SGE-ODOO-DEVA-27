4651e7895656555d486f265022deb1252feb2936
Davidup25 <unzagad@gmail.com>
2025-02-21 11:09:55 +0100

    Modificación del manifest y creación de modelos/categorias

---
+++ odoo/addons/dup_stock-deporte/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Gestión de Stock para Deportes",
+    'summary': "Módulo para gestionar stock de productos deportivos",
+    'description': "Este módulo permite gestionar el stock de productos deportivos en Odoo.",
+    'author': "DUP",
+    'category': 'Inventario',
+    'version': '1.0',
+    'depends': ['base', 'stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/producto_view.xml',
+        'views/categoria_view.xml',
+        'views/proveedor_view.xml',
+        'views/almacen_view.xml',
+        'views/menu.xml',
+    ],
+    'demo': ['demo/demo.xml'],
+    'installable': True,
+    'application': True,
+}
+
+
